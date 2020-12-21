138bccff4d5acd9c8a7c2bab3e8991e72b77d782
Janos Pasztor <janos@pasztor.at>
2020-12-21 16:37:08 +0100

    Build logs

--- schema/jobs.sql
+++ schema/jobs.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS refs;
 DROP TABLE IF EXISTS pulls;
+DROP TABLE IF EXISTS build_logs;
 DROP TABLE IF EXISTS jobs;
 CREATE TABLE jobs
 (
@@ -11,12 +12,16 @@ CREATE TABLE jobs
     completion_time DATETIME DEFAULT NULL COMMENT 'status.completionTime',
     url             VARCHAR(255) COMMENT 'status.url',
 
+    job_name_safe   VARCHAR(255),
+
     git_org         VARCHAR(255),
     git_repo        VARCHAR(255),
     git_repo_link   VARCHAR(255),
     git_base_ref    VARCHAR(255),
     git_base_sha    VARCHAR(255),
     git_base_link   VARCHAR(255),
+
+    artifacts_url VARCHAR(255),
 
     INDEX i_status (status)
 );
@@ -25,8 +30,10 @@ CREATE TABLE pulls
 (
     id          BIGINT PRIMARY KEY AUTO_INCREMENT,
     job_id      VARCHAR(255),
+    number      INT,
     author      VARCHAR(255),
     sha         VARCHAR(255),
+    pull_link   VARCHAR(255),
     commit_link VARCHAR(255),
     author_link VARCHAR(255),
 
@@ -35,5 +42,18 @@ CREATE TABLE pulls
         FOREIGN KEY (job_id)
             REFERENCES jobs (id)
             ON UPDATE CASCADE
-            ON DELETE RESTRICT
+            ON DELETE RESTRICT,
+    UNIQUE u_pulls (job_id, number, sha)
 );
+
+CREATE TABLE build_logs (
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    job_id      VARCHAR(255),
+    log         LONGBLOB,
+    UNIQUE i_job_id(job_id),
+    CONSTRAINT fk_build_logs_job_id
+        FOREIGN KEY fk_build_logs_job_id(job_id)
+            REFERENCES jobs (id)
+            ON UPDATE CASCADE
+            ON DELETE RESTRICT
+)
--- scrape_prow.go
+++ scrape_prow.go
@@ -1,14 +1,129 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
 	"context"
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"regexp"
+	"strings"
+	"sync"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	v1 "k8s.io/api/core/v1"
 )
+
+type JobMetadata struct {
+	Name              string    `json:"name"`
+	Namespace         string    `json:"namespace"`
+	SelfLink          string    `json:"selfLink"`
+	UID               string    `json:"uid"`
+	ResourceVersion   string    `json:"resourceVersion"`
+	Generation        int       `json:"generation"`
+	CreationTimestamp time.Time `json:"creationTimestamp"`
+}
+
+type JobSpec struct {
+	Type         string     `json:"type"`
+	Agent        string     `json:"agent"`
+	Cluster      string     `json:"cluster"`
+	Namespace    string     `json:"namespace"`
+	Job          string     `json:"job"`
+	Refs         JobRefs    `json:"refs"`
+	Report       bool       `json:"report"`
+	Context      string     `json:"context"`
+	RerunCommand string     `json:"rerun_command"`
+	PodSpec      v1.PodSpec `json:"pod_spec"`
+}
+
+type JobRefs struct {
+	Org      string    `json:"org"`
+	Repo     string    `json:"repo"`
+	RepoLink string    `json:"repo_link"`
+	BaseRef  string    `json:"base_ref"`
+	BaseSha  string    `json:"base_sha"`
+	BaseLink string    `json:"base_link"`
+	Pulls    []JobPull `json:"pulls"`
+}
+
+type JobStatus struct {
+	StartTime      *time.Time `json:"startTime,omitempty"`
+	PendingTime    *time.Time `json:"pendingTime,omitempty"`
+	CompletionTime *time.Time `json:"completionTime,omitempty"`
+	State          string     `json:"state"`
+	Description    string     `json:"description"`
+	URL            string     `json:"url"`
+	PodName        string     `json:"pod_name"`
+	BuildID        string     `json:"build_id"`
+}
+
+type JobPull struct {
+	Number     int    `json:"number"`
+	Author     string `json:"author"`
+	SHA        string `json:"sha"`
+	Link       string `json:"link"`
+	CommitLink string `json:"commit_link"`
+	AuthorLink string `json:"author_link"`
+}
+
+type Job struct {
+	Kind       string      `json:"kind"`
+	ApiVersion string      `json:"apiVersion"`
+	Metadata   JobMetadata `json:"metadata"`
+	Spec       JobSpec     `json:"spec"`
+	Status     JobStatus   `json:"status"`
+}
+
+type Jobs struct {
+	Items []Job `json:"items"`
+}
+
+func must(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func mustTX(err error, tx *sql.Tx) {
+	if err != nil {
+		_ = tx.Rollback()
+		log.Fatalln(err)
+	}
+}
+
+func fetchJobs() ([]Job, error) {
+	log.Println("Fetching jobs from Prow...")
+	url := "https://prow.ci.openshift.org/prowjobs.js?var=allBuilds"
+	client := http.Client{}
+	data, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	bytes, err := ioutil.ReadAll(data.Body)
+	if err != nil {
+		return nil, err
+	}
+	rawJSON := strings.Replace(string(bytes[:len(bytes)-1]), "var allBuilds = ", "", 1)
+
+	jobsRaw := &map[string]interface{}{}
+	if err := json.Unmarshal([]byte(rawJSON), jobsRaw); err != nil {
+		return nil, err
+	}
+
+	jobs := Jobs{}
+	if err := json.Unmarshal([]byte(rawJSON), &jobs); err != nil {
+		return nil, err
+	}
+	return jobs.Items, err
+}
 
 func main() {
 	mysqlHost := os.Getenv("MYSQL_HOST")
@@ -20,27 +135,132 @@ func main() {
 		"mysql",
 		fmt.Sprintf("%s:%s@tcp(%s)/%s", mysqlUser, mysqlPassword, mysqlHost, mysqlDB),
 	)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	must(err)
 
-	jobs, err := FetchJobs()
-	if err != nil {
-		log.Fatalf("%v", err)
+	//updateJobs(db)
+	//updateArtifactURLs(db)
+	updateBuildLogs(db)
+}
+
+func updateBuildLogs(db *sql.DB) {
+	res, err := db.Query(
+		"SELECT jobs.id, artifacts_url FROM jobs LEFT JOIN build_logs bl on jobs.id = bl.job_id WHERE artifacts_url IS NOT NULL AND bl.job_id IS NULL")
+	must(err)
+
+	lock := make(chan struct{}, 10)
+	wg := &sync.WaitGroup{}
+	for {
+		id := ""
+		url := ""
+		res.Next()
+		err := res.Scan(&id, &url)
+		if err != nil {
+			break
+		}
+		if url == "" {
+			continue
+		}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			lock <- struct{}{}
+			log.Printf("Fetching build log for job %s...\n", id)
+			response, err := http.Get(url + "build-log.txt")
+			must(err)
+			data, err := ioutil.ReadAll(response.Body)
+			must(err)
+			must(response.Body.Close())
+
+			gzipBytes := bytes.Buffer{}
+			gzipWriter := gzip.NewWriter(&gzipBytes)
+			_, err = gzipWriter.Write(data)
+			must(err)
+			must(gzipWriter.Flush())
+			must(gzipWriter.Close())
+
+			insertRes, err := db.Query(
+				"REPLACE INTO build_logs (job_id, log) VALUES (?, ?)",
+				id, gzipBytes.Bytes())
+			must(err)
+			must(insertRes.Close())
+			<-lock
+		}()
 	}
+	must(res.Close())
+	wg.Wait()
+}
+
+func updateArtifactURLs(db *sql.DB) {
+	res, err := db.Query("SELECT id, url FROM jobs WHERE artifacts_url IS NULL")
+	must(err)
+
+	artifactsRe := regexp.MustCompile(`<a href="(?P<url>[^"]+)">Artifacts</a>`)
+
+	lock := make(chan struct{}, 10)
+	wg := &sync.WaitGroup{}
+	for {
+		id := ""
+		url := ""
+		res.Next()
+		err := res.Scan(&id, &url)
+		if err != nil {
+			break
+		}
+		if url == "" {
+			continue
+		}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			lock <- struct{}{}
+			log.Printf("Fetching artifact URL for job %s...\n", id)
+			jobPage, err := http.Get(url)
+			must(err)
+			body, err := ioutil.ReadAll(jobPage.Body)
+			must(err)
+			matches := artifactsRe.FindStringSubmatch(string(body))
+			if len(matches) > 1 {
+				updateRes, err := db.Query(
+					"UPDATE jobs SET artifacts_url=? WHERE id=?", matches[1], id,
+				)
+				must(err)
+				must(updateRes.Close())
+			}
+			<-lock
+		}()
+	}
+	must(res.Close())
+	wg.Wait()
+}
+
+func updateJobs(db *sql.DB) {
+	jobs, err := fetchJobs()
+	must(err)
 
 	for _, job := range jobs {
 		tx, err := db.BeginTx(context.TODO(), &sql.TxOptions{})
-		if err != nil {
-			log.Fatalln(err)
+		must(err)
+		jobNameSafe := ""
+		if len(job.Spec.PodSpec.Containers) > 0 {
+			for _, env := range job.Spec.PodSpec.Containers[0].Env {
+				if env.Name == "JOB_NAME_SAFE" {
+					jobNameSafe = env.Value
+				}
+			}
 		}
-		if _, err := tx.Exec(
-			"REPLACE INTO jobs (" +
-				"id, job, status, start_time, pending_time, completion_time, url," +
-				"git_org, git_repo, git_repo_link, git_base_ref, git_base_sha, git_base_link" +
-				") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+		_, err = tx.Exec(
+			"INSERT INTO jobs ("+
+				"id, job, job_name_safe, status, start_time, pending_time, completion_time, url,"+
+				"git_org, git_repo, git_repo_link, git_base_ref, git_base_sha, git_base_link"+
+				") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"+
+				"ON DUPLICATE KEY UPDATE "+
+				"status = ?,"+
+				"start_time = ?,"+
+				"pending_time = ?,"+
+				"completion_time = ?",
 			job.Metadata.UID,
 			job.Spec.Job,
+			jobNameSafe,
 			job.Status.State,
 			job.Status.StartTime,
 			job.Status.PendingTime,
@@ -52,10 +272,38 @@ func main() {
 			job.Spec.Refs.BaseRef,
 			job.Spec.Refs.BaseSha,
 			job.Spec.Refs.BaseLink,
-		); err != nil {
-			_ = tx.Rollback()
-			log.Fatalln(err)
+			job.Status.State,
+			job.Status.StartTime,
+			job.Status.PendingTime,
+			job.Status.CompletionTime,
+		)
+		must(err)
+
+		//region Pulls
+		result, err := tx.Query("SELECT COUNT(*) AS cnt FROM pulls WHERE job_id=?", job.Metadata.UID)
+		mustTX(err, tx)
+		pulls := 0
+		result.Next()
+		err = result.Scan(&pulls)
+		mustTX(err, tx)
+		err = result.Close()
+		mustTX(err, tx)
+
+		if pulls == 0 {
+			for _, pull := range job.Spec.Refs.Pulls {
+				_, err = tx.Query(
+					"REPLACE INTO pulls ("+
+						"job_id, number, author, sha, pull_link, commit_link, author_link"+
+						") VALUES ("+
+						"?, ?, ?, ?, ?, ?, ?"+
+						")",
+					job.Metadata.UID, pull.Number, pull.Author, pull.SHA, pull.Link, pull.CommitLink, pull.AuthorLink,
+				)
+				mustTX(err, tx)
+			}
 		}
+		//endregion
+
 		if err := tx.Commit(); err != nil {
 			log.Fatalln(err)
 		}
